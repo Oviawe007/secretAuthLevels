98e42d90cb4ae0ccc28f0ce6aa0a373231f1d11d
Oviawe007 <josephoviawe4@gmail.com>
2022-09-20 02:53:58 +0100

    facbook register/login strategy added

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy= require('passport-facebook').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
@@ -36,6 +37,7 @@ const userSchema = new mongoose.Schema({
     email : String,
     password : String,
     googleId : String,
+    facebookId : String,
     secret : String
 });
 
@@ -71,6 +73,19 @@ passport.use(new GoogleStrategy({
     });
   }
 ));
+//
+passport.use(new FacebookStrategy({
+    clientID: process.env.CLIENT_ID_FB,
+    clientSecret: process.env.CLIENT_SECRET_FB,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+//
 //TODO
 
 app.get("/", function(req, res) {
@@ -88,7 +103,18 @@ app.get('/auth/google/secrets',
     res.redirect('/secrets');
 });
 //
+//
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
 
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secret.
+    res.redirect('/secrets');
+  });
+//
+//
 app.get("/login", function(req, res) {
     res.render("login");
 });
